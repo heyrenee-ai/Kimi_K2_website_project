c4cfefd74dc803c3ff7b6fd319ef8faade26408b
Renee <renee.mktg@gmail.com>
2025-09-03 00:15:14 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Kimi_K2_website_project',
+  base: '/Kimi_K2_website_project/',
 })
